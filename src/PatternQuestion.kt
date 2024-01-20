4e0b2e6257eb48376cb9f6e1ea20956be2d046e2
Khaleeq Ahmad <khaleeqahmad2022@outlook.com>
2024-01-20 20:58:10 +0500

    Initial commit

---
+++ src/PatternQuestion.kt
@@ -0,0 +1,23 @@
+
+fun solidRectangle() {
+
+/*  Using Repeat Function
+
+    repeat(4) {
+        print("*")
+        repeat(3) {
+            print("*")
+        }
+        println()
+    }*/
+
+
+    for (i in 1..4) {
+        print("*")
+        for (j in 1..3) {
+            print("*")
+        }
+        println()
+    }
+
+}
