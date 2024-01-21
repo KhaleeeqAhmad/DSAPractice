d8619aad3a86df3622797663ef6881844289bb3f
Khaleeq Ahmad <khaleeqahmad2022@outlook.com>
2024-01-21 14:24:45 +0500

    hollowRectangle Question.

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    solidRectangle()
+    //solidRectangle(3,3)
+    hollowRectangle()
 }
--- src/PatternQuestion.kt
+++ src/PatternQuestion.kt
@@ -1,23 +1,28 @@
 
-fun solidRectangle() {
+fun solidRectangle(numberOfRows: Int, numberOfColumns: Int) {
 
-/*  Using Repeat Function
-
-    repeat(4) {
-        print("*")
-        repeat(3) {
-            print("*")
-        }
-        println()
-    }*/
-
-
-    for (i in 1..4) {
-        print("*")
-        for (j in 1..3) {
+    for (i in 0..numberOfRows) {
+        for (j in 0..numberOfColumns) {
             print("*")
         }
         println()
     }
 
 }
+
+
+fun hollowRectangle() {
+    val n = 4
+    val m = 5
+    for (i in 1..n) {
+        for (j in 1..m) {
+            if (i == 1 || j==1 || i==n || j == m) {
+                print("*")
+            } else {
+                print(" ")
+            }
+        }
+        println()
+    }
+}
+
