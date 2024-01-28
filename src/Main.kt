dcbaa3934016b0f67271bef4ea5a9dd20899cf07
Khaleeq Ahmad <khaleeqahmad2022@outlook.com>
2024-01-28 20:06:45 +0500

    problem #17 maxMinNumberInArray

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,13 @@
+import arrays.maxMinNumberInArray
 import arrays.searchNumberFromArray
 import basic_questions.greaterNumber
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-
     val tempArray = intArrayOf(1,4,6,8,3,5,9, 1)
-    searchNumberFromArray(tempArray, 1)
+    val emptyArray = intArrayOf()
+
+
+    maxMinNumberInArray(tempArray)
 }
--- src/arrays/ArrayQuestion.kt
+++ src/arrays/ArrayQuestion.kt
@@ -1,5 +1,9 @@
 package arrays
 
+import javax.management.openmbean.ArrayType
+import kotlin.math.max
+import kotlin.math.min
+
 fun searchNumberFromArray(array: IntArray, number: Int) {
 
     if (array.isEmpty()) {
@@ -11,4 +15,31 @@ fun searchNumberFromArray(array: IntArray, number: Int) {
             println(i)
         }
     }
+}
+
+/*
+* Find the maximum & minimum number in an array of integers.
+* */
+
+fun maxMinNumberInArray(array: IntArray) {
+
+    if (array.isEmpty()) {
+        println("Given Array is empty")
+        return
+    }
+
+    var minNumberInArray = array[0]
+    var maxNumberInArray = array[0]
+
+    for (i in array.indices) {
+        if (maxNumberInArray < array[i]) {
+            maxNumberInArray = array[i]
+        }
+
+        if (minNumberInArray > array[i]) {
+            minNumberInArray = array[i]
+        }
+    }
+
+    println("Min Number is $minNumberInArray and Max Number is $maxNumberInArray")
 }
