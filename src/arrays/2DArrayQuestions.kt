69105015dee68419a13e40c654123721ca879e20
Khaleeq Ahmad <khaleeqahmad2022@outlook.com>
2024-01-29 23:22:52 +0500

    2DArray

--- src/arrays/2DArrayQuestions.kt
+++ src/arrays/2DArrayQuestions.kt
@@ -2,11 +2,11 @@ package arrays
 
 fun array2D(rows: Int, columns: Int) {
     val twoDArray = Array(rows) { Array(columns) { i -> i * 1 } }
-    println(twoDArray.contentDeepToString())
 
     for (i in 0..<rows) {
         for (j in 0..<columns) {
-            print(twoDArray[i][j], )
+            print("${twoDArray[i][j]} ")
         }
+        println()
     }
 }
